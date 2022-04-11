cb69bb10afa2dfec1e84e2d26e840c3d30255986
Monstpeet <100101481+Monstpeet@users.noreply.github.com>
2022-04-11 23:32:58 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("Header");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+    
+  } else {
+    header.classList.remove("sticky");
+    header.style.backgroundColor="white"
+  }
+}
